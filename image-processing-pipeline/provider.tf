e357f70e7c357681e78a503df5781dc980011344
Petko Kolev <petkokolev@Petkos-MacBook-Pro.local>
2026-04-24 16:32:16 +0100

    fix import existing lambda event source mapping into terraform state

--- image-processing-pipeline/provider.tf
+++ image-processing-pipeline/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "petko-terraform-state-image"
+    key            = "image-processor/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
