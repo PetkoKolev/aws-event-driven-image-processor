b27bee956215be504384a288f583b3880099fb73
Petko Kolev <petkokolev@Petkos-MacBook-Pro.local>
2026-04-24 19:36:10 +0100

    Refactor: split lambdas into separate packages | Previously were in one folder and packaged together

--- image-processing-pipeline/lambda.tf
+++ image-processing-pipeline/lambda.tf
@@ -1,8 +1,11 @@
+# =========================
+# Image Processor Lambda
+# =========================
 resource "aws_lambda_function" "image_processor" {
   function_name = "image-processor"
 
-filename         = "${path.module}/lambda/function.zip"
-source_code_hash = filebase64sha256("${path.module}/lambda/function.zip")
+  filename         = "${path.module}/image_processor.zip"
+  source_code_hash = filebase64sha256("${path.module}/image_processor.zip")
 
   handler = "lambda_function.lambda_handler"
   runtime = "python3.11"
@@ -12,6 +15,26 @@ source_code_hash = filebase64sha256("${path.module}/lambda/function.zip")
   timeout = 10
 }
 
+# =========================
+# DLQ Reprocessor Lambda
+# =========================
+resource "aws_lambda_function" "dlq_reprocessor" {
+  function_name = "dlq-reprocessor"
+
+  filename         = "${path.module}/dlq_reprocessor.zip"
+  source_code_hash = filebase64sha256("${path.module}/dlq_reprocessor.zip")
+
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.11"
+
+  role = aws_iam_role.lambda_role.arn
+
+  timeout = 10
+}
+
+# =========================
+# SQS → Image Processor
+# =========================
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.image_queue.arn
   function_name    = aws_lambda_function.image_processor.arn
@@ -19,10 +42,34 @@ resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   batch_size = 1
 }
 
+# =========================
+# SQS → DLQ Reprocessor
+# =========================
+resource "aws_lambda_event_source_mapping" "dlq_trigger" {
+  event_source_arn = aws_sqs_queue.dlq.arn
+  function_name    = aws_lambda_function.dlq_reprocessor.arn
+
+  batch_size = 1
+}
+
+# =========================
+# Permissions (Image Processor)
+# =========================
 resource "aws_lambda_permission" "allow_sqs" {
   statement_id  = "AllowExecutionFromSQS"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.image_processor.function_name
   principal     = "sqs.amazonaws.com"
   source_arn    = aws_sqs_queue.image_queue.arn
+}
+
+# =========================
+# Permissions (DLQ Reprocessor)
+# =========================
+resource "aws_lambda_permission" "allow_dlq" {
+  statement_id  = "AllowExecutionFromDLQ"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dlq_reprocessor.function_name
+  principal     = "sqs.amazonaws.com"
+  source_arn    = aws_sqs_queue.dlq.arn
 }
