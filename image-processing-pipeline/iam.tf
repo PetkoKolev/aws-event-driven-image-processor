054aafaa3dde0dc9e6f5cac6ea1f04a11d239336
Petko Kolev <petkokolev@Petkos-MacBook-Pro.local>
2026-04-29 22:25:40 +0100

    fixed iam role typo

--- image-processing-pipeline/iam.tf
+++ image-processing-pipeline/iam.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:PutObject"
+          "s3:PutObject",
           "s3.HeadObject"
         ]
         Resource = "${aws_s3_bucket.images.arn}/*"
